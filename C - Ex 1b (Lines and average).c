d3e2e995b9401172ff2f3f788ef6c96cd4974dcf
ryankeh <113022074+ryankeh@users.noreply.github.com>
2022-10-25 15:51:04 +0800

    Update C - Ex 1b (Lines and average).c

--- C - Ex 1b (Lines and average).c
+++ C - Ex 1b (Lines and average).c
@@ -1,19 +1,25 @@
 #include <stdio.h>
 int main()  
 {
-    int integerinput=0, integersum=0, lines, linesinitial=0;
-    printf("Number of lines: ")
-    scanf("%d", &lines)
+    int integerinput=0, integersum=0, lines, i, j, denominator=0;
+    float average;
+    printf("Number of lines: ");
+    scanf("%d", &lines);
     
-    for (linesinitial; lines)
-    printf("Enter integer numbers:");
-    while (integerinput != -1)  
-    {
-    printf("");
-    scanf("%d", &integerinput);
-    integersum = integersum + integerinput;
-    }
-    printf("%d", integersum+1);
-    
+    for (i=0; i<lines; i++){
+        j=i+1;
+        printf("Enter line %d (end with -1):", j);
+        while (integerinput != -1)
+            {
+            scanf("%d", &integerinput);
+            denominator = denominator+1;
+            integersum = integersum + integerinput;
+            }
+        average = (float)(integersum+1) / (float)(denominator-1);
+        printf("Average = %0.2f \n", average);
+        integerinput=0;
+        integersum=0;
+        denominator=0;
+        }
     return 0;
 }
