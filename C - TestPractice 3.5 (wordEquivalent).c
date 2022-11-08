79121b2191e6bf3347f229b85ccddd12573c5ce2
Ryan Keh <113022074+ryankeh@users.noreply.github.com>
2022-11-08 20:54:09 +0800

    Update C - TestPractice 3.5 (wordEquivalent).c
    too take care of cases above 99

--- C - TestPractice 3.5 (wordEquivalent).c
+++ C - TestPractice 3.5 (wordEquivalent).c
@@ -16,9 +16,10 @@ void wordEquivalent(int num, char *str)
 {
 	/*edit*/
    /* Write your code here */
-int a,b,i;
+   int a,b,i;
    char stra[40],strb[40]="";
    char str0[10][6]={{'z','e','r','o'},{'o','n','e'},{'t','w','o'},{'t','h','r','e','e'},{'f','o','u','r'},{'f','i','v','e'},{'s','i','x'},{'s','e','v','e','n'},{'e','i','g','h','t'},{'n','i','n','e'}};
+   char toohigh[40]="Input exceeds 99";
    char blankstr[40]=" ";
    a=num%10;
    b=num/10;
@@ -32,8 +33,14 @@ int a,b,i;
            strcpy(strb,str0[i]);
        }
    }
-   strcat(str,stra);
-   strcat(str,blankstr);
-   strcat(str,strb);
+   if(num>99){
+       strcpy(str,toohigh);
+   }
+   else{
+       strcat(str,stra);
+       strcat(str,blankstr);
+       strcat(str,strb);
+   }
+
 	/*end_edit*/
 }
