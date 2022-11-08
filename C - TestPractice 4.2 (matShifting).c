1d235df84bdb5e8a9d55c4442daf01ebd4978be4
Ryan Keh <113022074+ryankeh@users.noreply.github.com>
2022-11-08 21:27:03 +0800

    Update C - TestPractice 4.2 (matShifting).c

--- C - TestPractice 4.2 (matShifting).c
+++ C - TestPractice 4.2 (matShifting).c
@@ -1,33 +1,38 @@
 #include <stdio.h>
 #define M 10
-int matTriSum(int x[M][M], int n);
+void matShifting(int a[M][M], int b[M][M], int n);
 int main()
 {
-   int x[M][M];
-   int n,i,j,s;
+   int a[M][M], b[M][M];
+   int n,i,j;
    
    printf("Enter array (nxn) size (n<=10): \n");
    scanf("%d",&n);
-   for (i=0;i<n;i++) {
-      printf("Enter row %d: \n",i);
-      for (j=0;j<n;j++)
-         scanf("%d",&x[i][j]);
+   for (i=0; i<n; i++) {
+      printf("Enter row %d: \n", i);
+      for (j=0; j<n; j++)
+         scanf("%d",&a[i][j]);
    }
-   s=matTriSum(x,n);
-   printf("The sum is: %d\n",s);
+   matShifting(a,b,n);
+   printf("Array b: \n");
+   for (i=0;i<n;i++) {
+      for (j=0;j<n;j++)
+         printf("%d ",b[i][j]);
+      printf("\n");
+   }
    return 0;
 }
-int matTriSum(int x[M][M], int n)
+void matShifting(int a[M][M], int b[M][M], int n)
 {
 	/*edit*/
    /* Write your code here */
-   int i, j, sum=0;
+   int i, j;
    for(i=0;i<n;i++){
-       for(j=i;j<n;j++){
-           sum+=x[i][j];
+       for(j=1;j<n;j++){
+           b[i][j]=a[i][j-1];
        }
+       b[i][0]=a[i][n-1];
    }
-   return sum;
 
 
 	/*end_edit*/
